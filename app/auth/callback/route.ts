e6d12fc3c0a5f0a2b708ecf3b72b426be0182655
jofflin <tech@hoefflin.io>
2024-06-14 19:30:46 +0200

    rollback

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -14,6 +14,7 @@ export async function GET(request: Request) {
     await supabase.auth.exchangeCodeForSession(code);
   }
 
+  console.log("ORIGIN:", origin);
   // URL to redirect to after sign up process completes
-  return NextResponse.redirect(`https://flunky.hoefflin.io`);
+  return NextResponse.redirect(`${origin}`);
 }
